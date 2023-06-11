1a1308e28b3fbae40e7b66accdac9bbdaaa5b6aa
Notebook <magog21@gmail.com>
2023-06-11 23:08:08 +0300

    HomeWorkLessons20

---
+++ src/main/java/by/radevich/helloservlet/homework/helloservlethomework/DateExamption.java
@@ -0,0 +1,31 @@
+package by.radevich.helloservlet.homework.helloservlethomework;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateExamption extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LocalDate date = LocalDate.now();
+        PrintWriter writer = resp.getWriter();
+        writer.println(date);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the date");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date1 = LocalDate.parse(scanner.nextLine(), formatter);
+        DayOfWeek dayOfWeek = date1.getDayOfWeek();
+        writer.println(dayOfWeek);
+        writer.println(date.getDayOfYear());
+
+    }
+}
